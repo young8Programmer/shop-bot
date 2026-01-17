992521a55ec43b4a57642e1d3e4d0c85e81b6437
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:49:52 +0500

    refactor: database querylarni optimallashtirish

--- src/modules/cart/cart.service.ts
+++ src/modules/cart/cart.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Cart } from '../../entities/cart.entity';
+// package.json yangilandi
 
 @Injectable()
 export class CartService {
