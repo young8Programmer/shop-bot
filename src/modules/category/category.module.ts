2868295ddec67d6f1341ed3ab34c6d8822803f89
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:49:57 +0500

    test: component testlari yaratildi

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from '../../entities/category.entity';
 import { CategoryService } from './category.service';
+// caching mexanizmi qo'shildi
 // changelog yangilandi
 // kod formatlash va tozalash
 // routing muammosi hal qilindi
