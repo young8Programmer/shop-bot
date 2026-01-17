ee4a6ed249d0e96c6e435575f582ae6b7729ec53
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:01 +0500

    docs: API hujjatlarini qo'shish

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 // package.json yangilandi
+// database migrations yaratildi
 import { Category } from '../../entities/category.entity';
 // unit testlar qo'shildi
 
