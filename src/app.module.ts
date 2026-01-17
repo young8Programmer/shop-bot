edc55f9e2832f1fee27521fb4ac98c90ae9e4681
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:48:55 +0500

    perf: bundle size optimallashtirildi

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { I18nModule, QueryResolver, AcceptLanguageResolver } from 'nestjs-i18n';
 // CI/CD pipeline sozlandi
+// API response formatini yaxshilash
 import { join } from 'path';
 // integration testlar yaratildi
 // code comments qo'shildi
