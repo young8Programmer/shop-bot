fd1f7cc6f3a824e0b285ee808b5324b929429c64
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:49:45 +0500

    test: unit testlar qo'shildi

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 // error handling yaxshilandi
+// middleware funksiyalari qo'shildi
   await app.listen(3000);
 }
 bootstrap();
