19a8274eedda682f58e2bcb8b23ae986ae7b5633
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:48:50 +0500

    docs: installation qo'llanmasi yaratildi

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,7 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
+// user authentication qo'shildi
 export class AppController {
 // middleware funksiyalari qo'shildi
   constructor(private readonly appService: AppService) {}
