a0ac905d5ccd169d49b13171a4f73902fad07f93
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:19 +0500

    docs: API hujjatlarini qo'shish

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from '../../entities/product.entity';
 import { ProductService } from './product.service';
+// installation qo'llanmasi yaratildi
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
