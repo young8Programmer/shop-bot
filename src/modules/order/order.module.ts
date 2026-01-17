0c5470227caee704056e8e1665486ee55d4fbb04
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:12 +0500

    docs: README faylini yangilash

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -2,6 +2,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from '../../entities/order.entity';
+// API response formatini yaxshilash
 import { OrderService } from './order.service';
 import { User } from 'src/entities/user.entity';
 import { Product } from 'src/entities/product.entity';
