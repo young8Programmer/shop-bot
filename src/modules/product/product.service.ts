d5e96a8927d1395bb0138c03a8a7f71c72e54a50
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:23 +0500

    style: prettier formatlash

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+// type error tuzatildi
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from '../../entities/product.entity';
