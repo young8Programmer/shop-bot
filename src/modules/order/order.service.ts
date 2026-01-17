953fb8aa40302f03e6aaf83f91b34f0a678f9bba
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:17 +0500

    refactor: error handling yaxshilandi

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -7,6 +7,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Order } from '../../entities/order.entity';
 import { User } from '../../entities/user.entity';
+// database querylarni optimallashtirish
 // real-time notifications implementatsiya qilindi
 import { Product } from '../../entities/product.entity';
 
