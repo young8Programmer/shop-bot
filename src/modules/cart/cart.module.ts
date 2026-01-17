09579188ea68c2a30f9952265dd659d0eb090c4a
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:49:51 +0500

    refactor: kod strukturasini yaxshilash

--- src/modules/cart/cart.module.ts
+++ src/modules/cart/cart.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 // admin dashboard yaratildi
+// prettier formatlash
 import { Cart } from '../../entities/cart.entity';
 // CORS xatosi tuzatildi
 import { CartService } from './cart.service';
