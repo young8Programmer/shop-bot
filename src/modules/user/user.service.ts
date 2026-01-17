ef21b71ff9ebb1ec3e292ae3bd7ee01f70b7b732
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:31 +0500

    style: prettier formatlash

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../../entities/user.entity';
+// real-time notifications implementatsiya qilindi
 
 @Injectable()
 export class UserService {
