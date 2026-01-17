4fd27db300cc508565b8ab2acf73d4a84812fd7a
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:38 +0500

    test: component testlari yaratildi

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,3 +1,4 @@
+// database querylarni optimallashtirish
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
