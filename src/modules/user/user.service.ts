9c0d1fabf142e06132c104536a411085b7a0fa86
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:32 +0500

    docs: changelog yangilandi

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../../entities/user.entity';
 // authentication xatosi tuzatildi
+// dependencies yangilandi
 // real-time notifications implementatsiya qilindi
 
 @Injectable()
