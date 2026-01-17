6dd7d19d3b9180f976f5d0b31043b0b2fdf524f1
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:34 +0500

    test: database testlari qo'shildi

--- src/types/bot.context.ts
+++ src/types/bot.context.ts
@@ -1,12 +1,10 @@
 import * as TelegramBot from 'node-telegram-bot-api';
-
 export interface SessionData {
   phone?: string;
   address?: string;
   deliveryType?: string;
   page?: number;
 }
-
 export interface CustomContext {
   message?: TelegramBot.Message;
   chat?: TelegramBot.Chat;
@@ -14,7 +12,6 @@ export interface CustomContext {
   match?: RegExpExecArray;
   session: SessionData;
 }
-
 export interface Sessions {
   [key: number]: SessionData;
 }
