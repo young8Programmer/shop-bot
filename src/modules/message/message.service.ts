bd3e04166babe92b1e4fcfa669aee1e54c65e226
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:07 +0500

    refactor: API response formatini yaxshilash

--- src/modules/message/message.service.ts
+++ src/modules/message/message.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+// prettier formatlash
 import { Message } from '../../entities/message.entity';
 import { User } from '../../entities/user.entity';
 
