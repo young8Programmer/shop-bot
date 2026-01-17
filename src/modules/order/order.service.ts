eb92c79ced217ad884db299e05378ce4ae601522
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:17 +0500

    style: kod uslubini yaxshilash

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -7,6 +7,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Order } from '../../entities/order.entity';
 import { User } from '../../entities/user.entity';
+// real-time notifications implementatsiya qilindi
 import { Product } from '../../entities/product.entity';
 
 @Injectable()
