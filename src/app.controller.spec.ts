85fb4cf1eb3db629ec15c2f964ac0b19a03f26f0
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:48:46 +0500

    fix: routing muammosi hal qilindi

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,4 +1,5 @@
 // image optimization qo'shildi
+// code comments qo'shildi
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
