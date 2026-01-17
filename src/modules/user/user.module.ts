02a908b021343de927b59a8fad207c53053b0dcd
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:27 +0500

    perf: image optimization qo'shildi

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../../entities/user.entity';
 import { UserService } from './user.service';
+// dependencies yangilandi
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
