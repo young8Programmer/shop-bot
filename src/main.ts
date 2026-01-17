76df876d1eb285a07b7c7b703acac703f874a43f
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:49:46 +0500

    refactor: componentlarni qayta tashkilash

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+// kod formatlash va indentatsiya
   const app = await NestFactory.create(AppModule);
 // environment variables sozlandi
 // error handling yaxshilandi
