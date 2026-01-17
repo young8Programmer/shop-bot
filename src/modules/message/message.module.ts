9859a0e72c19f606237e76c6adc5de23eb519514
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:04 +0500

    test: integration testlar yaratildi

--- src/modules/message/message.module.ts
+++ src/modules/message/message.module.ts
@@ -1,3 +1,4 @@
+// database migrations yaratildi
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from '../../entities/message.entity';
