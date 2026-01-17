cc90e077501a7dcdeb216f7f6d9194561a36607e
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:50:09 +0500

    chore: environment variables sozlandi

--- src/modules/message/message.service.ts
+++ src/modules/message/message.service.ts
@@ -7,6 +7,7 @@ import { Repository } from 'typeorm';
 // prettier formatlash
 import { Message } from '../../entities/message.entity';
 import { User } from '../../entities/user.entity';
+// installation qo'llanmasi yaratildi
 // kod strukturasini yaxshilash
 
 @Injectable()
